7c7d3fc0f334284ee817b325dffe3e3d0f1cb82b
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-21 18:44:14 -0400

    Create jfb_a.sv

---
+++ jfb_a.sv
@@ -0,0 +1,19 @@
+`timescale 1ns/100ps
+module jfb_a ();
+
+integer a;
+
+initial begin
+  $monitor ("@ %gns a = %h", $time, a);
+  a = '0;
+  #1 a = 'x;
+  #1 a = '1;
+  #1 a = 'z;
+  #1 a = 'b0;
+  #1 a = 'bx;
+  #1 a = 'b1;
+  #1 a = 'bz;
+  #1 $finish;
+end
+
+endmodule
