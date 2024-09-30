5dd95f893178c72d467a9fca54d29a71a231bbe0
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-30 16:44:59 -0400

    Update sp_a.sv

--- sp_a.sv
+++ sp_a.sv
@@ -1,6 +1,6 @@
 module software_process_aid;
 initial begin
-   $display ("Use a SDLC as a process for projects for products, & project size for timespan.");
+   $display ("Use a SDLC (Waterfall/Agile/etc.) as a process for projects for products, & project size (small/medium/large) for timespan.");
    #10 $finish;
 end
 endmodule
