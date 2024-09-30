c4183e0357174e84848f453a101f8a5498231ead
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-30 16:41:46 -0400

    Update spm_a.sv

--- spm_a.sv
+++ spm_a.sv
@@ -1,6 +1,6 @@
 module software_project_management_aid;
 initial begin
-   $display ("Use a SDLC as a process for projects for products");
+   $display ("Use a SDLC as a process for projects for products, & project size for timespan.");
    #10 $finish;
 end
 endmodule
