f445dcca0aa28a5e2fb5d22badbdf6b03c7ce2d8
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-30 16:43:15 -0400

    Update and rename spm_a.sv to sp_a.sv

--- sp_a.sv
+++ sp_a.sv
@@ -1,4 +1,4 @@
-module software_project_management_aid;
+module software_process_aid;
 initial begin
    $display ("Use a SDLC as a process for projects for products, & project size for timespan.");
    #10 $finish;
