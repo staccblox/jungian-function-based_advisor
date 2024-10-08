bdd3e46a0cff2f293eeabdf168a3820362bd47c6
staccblox <67884277+staccblox@users.noreply.github.com>
2024-10-08 12:02:40 -0400

    Update sp_a.sv

--- sp_a.sv
+++ sp_a.sv
@@ -1,6 +1,7 @@
 module software_process_aid;
 initial begin
-   $display ("Use a SDLC (Waterfall/Agile/etc.) as a process for projects for products, & project size (small/medium/large) for timespan.");
+   $display ("Use a SDLC (Waterfall/Agile/etc.) as a process for projects for products, & project size (small/medium/large) for timespan. ");
+   $display ("Diagrams by modeling spaces may be recommended: problem (use case & activity) during planning, solution (class & sequence) during analysis & design, and architectural (component & deployment) during implementation/etc. ");
    #10 $finish;
 end
 endmodule
